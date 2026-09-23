a64f6b8446b72ff683d680c2a4a240c29f64ea99
germanggs <germangarvan@gmail.com>
2026-09-23 16:29:02 +0200

    Comit de nuevo

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -4,9 +4,9 @@
 from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\EODP\eodp_students\auxiliary'
-indir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-L1B\input"
-outdir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-L1B\myoutputs"
+auxdir = r'C:\\EODP_test_repo\\EODP_test_repo\\auxiliary'
+indir = r'C:\\EODP_test_repo\\EODP_test_repo\\EODP_TER_2021\\EODP-TS-L1B\\input'
+outdir = r'C:\\EODP_test_repo\\EODP_test_repo\\EODP_TER_2021\\EODP-TS-L1B\\Output_German_2'
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
